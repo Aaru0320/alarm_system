eb49ab43a9422b062bbdce243d20f99d5f2393df
Aaru0320 <aarushivishwakarma65@gmail.com>
2024-09-13 11:43:01 +0530

    initial commit

---
+++ email_settings.py
@@ -0,0 +1,4 @@
+password = "vabs hxcn dwll hwfe"
+from_email = "aarushivishwakarma65@gmail.com"  # must match the email used to generate the password
+to_email = "kunal@xynocast.com"  # receiver email
+#to_email = "arvindcbg@gmail.com"  # receiver email
